1d5743780ecfe66d8433a884365c21347c0b7bf9
Miguel Castro <miguelcastro@mk-is.pt>
2012-04-30 15:58:01 +0100

    - Initial commit

---
+++ src/java/dai/DAI_HARD.java
@@ -0,0 +1,340 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dai;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Mk-is
+ */
+@WebService(serviceName = "DAI_HARD")
+public class DAI_HARD {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "US11")
+    public String US11() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US12")
+    public String US12() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US13")
+    public String US13() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US21")
+    public String US21() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US221")
+    public String US221() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US222")
+    public String US222() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US22")
+    public String US22() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    
+    @WebMethod(operationName = "US231")
+    public String US231() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+     @WebMethod(operationName = "US232")
+    public String US232() {
+        //TODO write your implementation code here:
+        return null;
+    }
+     
+    @WebMethod(operationName = "US31")
+    public String US31() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+     @WebMethod(operationName = "US32")
+    public String US32() {
+        //TODO write your implementation code here:
+        return null;
+    }
+     
+      @WebMethod(operationName = "US33")
+    public String US33() {
+        //TODO write your implementation code here:
+        return null;
+    }
+      
+    @WebMethod(operationName = "US34")
+    public String US34() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US351")
+    public String US351() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US411")
+    public String US411() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US412")
+    public String US412() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US413")
+    public String US413() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US414")
+    public String US414() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US421")
+    public String US421() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US422")
+    public String US422() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US431")
+    public String US431() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US432")
+    public String US432() {
+        //TODO write your implementation code here:
+        return null;
+    }
+   
+    
+     @WebMethod(operationName = "US441")
+    public String US441() {
+        //TODO write your implementation code here:
+        return null;
+    }
+     
+      @WebMethod(operationName = "US442")
+    public String US442() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US45")
+    public String US45() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US511")
+    public String US511() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US512")
+    public String US512() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US521")
+    public String US521() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US522")
+    public String US522() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US523")
+    public String US523() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+     @WebMethod(operationName = "US5311")
+    public String US5311() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5312")
+    public String US5312() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5313")
+    public String US5313() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US541")
+    public String US541() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US542")
+    public String US542() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5611")
+    public String US5611() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5612")
+    public String US5612() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5613")
+    public String US5613() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5614")
+    public String US5614() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+     
+    @WebMethod(operationName = "US5621")
+    public String US5621() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5622")
+    public String US5622() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5623")
+    public String US5623() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5631")
+    public String US5631() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5632")
+    public String US5632() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US571")
+    public String US571() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+     @WebMethod(operationName = "US5721")
+    public String US5721() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5722")
+    public String US5722() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+     @WebMethod(operationName = "US5731")
+    public String US5731() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5732")
+    public String US5732() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+     @WebMethod(operationName = "US5741")
+    public String US5741() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    @WebMethod(operationName = "US5742")
+    public String US5742() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+     @WebMethod(operationName = "US5743")
+    public String US5743() {
+        //TODO write your implementation code here:
+        return null;
+    }
+        
+        
+
+    
+    
+}
---
+++ src/java/dai/Fachada.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dai;
+
+/**
+ *
+ * @author Mk-is
+ */
+public class Fachada {
+    
+}
